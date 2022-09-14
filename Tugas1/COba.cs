00b8e951cf2e8dd496dc53d8b5c12f57592d3456
kayubakar0 <suryaadrian970@gmail.com>
2022-09-14 17:06:12 +0700

    Tugas 1

---
+++ Tugas1/COba.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class COba : MonoBehaviour
+{
+    public string myName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("My name is "+myName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
